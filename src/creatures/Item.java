5a5fce35f0f34012a92f18a8ff59c462bbf15ddd
mafagafogigante <mafagafogigante@gmail.com>
2014-08-03 14:52:35 -0300

    started implementing inventory

---
+++ src/creatures/Item.java
@@ -0,0 +1,25 @@
+package creatures;
+
+public class Item {
+
+    private String name;
+    private int weight;
+    
+    public Item(String name) {
+        this.name = name;
+    }
+    
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+    
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
